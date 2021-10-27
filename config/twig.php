bb336bfae92da84663ad0787650813a43c4b0883
Xavier CHAVAUDREY <chavaudreyxav@hotmail.fr>
2021-10-27 21:52:01 +0200

    first commit

--- config/twig.php
+++ config/twig.php
@@ -6,3 +6,5 @@ $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
 
+$contactEmail = 'dupondleon@gmail.com';
+$twig->addGlobal('contactEmail', $contactEmail);
